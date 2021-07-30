9f94138edef3d0f07b59703b85448bd8e30569ed
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-07-30 15:25:53 -0300

    Criado componente Wallet e adicionado na rota /carteira conforme especificado no requisito 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './reducers/wallet';
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={ Login } />
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
     </div>
   );
 }
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react';
+
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+class Wallet extends Component {
+  render() {
+    return (
+      <div>Wallet</div>
+    );
+  }
+}
+
+export default Wallet;
